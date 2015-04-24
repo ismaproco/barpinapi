13727ac9093a28c1a00d35e0bb51abee12c69f06
Ismael Jimenez <ijimenezgarzon@gmail.com>
2015-04-23 23:00:00 -0600

    Updated routes file

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,17 @@
-// dependencies 
+// dependencies
 var express = require('express');
 var User = require('../models/users');
+var News = require('../models/news');
 
 var router = express.Router();
 
 // Api user properties
 User.methods(['get','put','post','delete']);
 User.register(router, '/users');
+
+// Api news properties
+News.methods(['get','put','post','delete']);
+News.register(router, '/news');
 
 // return the application router
 module.exports = router;
